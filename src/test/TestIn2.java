8ecfff79696b463de9ca50caac9853e88c309f15
DiyangZou <dzou@smu.edu>
2020-06-29 15:46:15 -0500

    06290346

--- src/test/TestIn2.java
+++ src/test/TestIn2.java
@@ -1,11 +1,17 @@
 package test;
 
-import java.util.List;
 
-public class TestIn2 extends Testin{
+public class TestIn2{
 	
-	public int test(List<?> nums) {
-		return 0;
+	public int test() {
+		int k = -1;
+		int b = k % 3;
+		return b;
+	}
+	
+	public static void main(String[] args) {
+		TestIn2 s = new TestIn2();
+		System.out.print(s.test());
 	}
 
 }
